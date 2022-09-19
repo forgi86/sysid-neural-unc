e8e0abc392ada6f917006025e5267ae07aa8e8a3
marco <marco.forgione@supsi.ch>
2022-09-19 15:05:36 +0200

    fixed figures

--- examples/wh2009/00_wh_generate_train_ms.py
+++ examples/wh2009/00_wh_generate_train_ms.py
@@ -5,7 +5,7 @@ import torch
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from models import WHSys
+from true_system import WHSys
 import control
 from common_input_signals import multisine
 
