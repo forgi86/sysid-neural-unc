fixed bug in std_unc computation

examples/wh2009/02_wh_compute_cov_matrix_slow.py
@@ -79,7 +79,7 @@ if __name__ == '__main__':
 
     loss = torch.mean((y_sim - y_v)**2)
 
-    # Exact Hessian computation
+    # Make model functional
     func_model, params = functorch.make_functional(model)
 
     # Naive jacobian computation